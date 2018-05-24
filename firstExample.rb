6010820a55ca05f14c10999cf9675561b266f22a
Alexei <alexeim763@gmail.com>
2018-05-24 15:42:22 -0500

    fixed

--- firstExample.rb
+++ firstExample.rb
@@ -6,3 +6,19 @@ str.chomp!
 temp, scale = str.split(" ")
 
 abort "#{temp} is not a valid number." if temp !~ /-?\d+/
+
+temp = temp.to_f
+case scale
+    when "C", "c"
+        f = 1.8*temp + 32
+    when "F", "f"
+        c = (5.0/9.0)*(temp/32)
+else
+    abort "Must specify C or F"
+end
+
+if f.nil?
+    puts "#{c} degrees C"
+else
+    puts "#{f} degrees F"
+end
