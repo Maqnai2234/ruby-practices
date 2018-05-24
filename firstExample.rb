ede944166d6c3964a9c228e012d0e9dbf7135d05
Alexei <alexeim763@gmail.com>
2018-05-24 15:39:14 -0500

    add expresion regular

--- firstExample.rb
+++ firstExample.rb
@@ -2,3 +2,7 @@ print "please enter a temperature and scale (C or F)"
 STDOUT.flush
 str = gets
 exit if str.nil? || str.empty?
+str.chomp!
+temp, scale = str.split(" ")
+
+abort "#{temp} is not a valid number." if temp !~ /-?\d+/
