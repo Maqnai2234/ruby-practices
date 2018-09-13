9adb484cfb0ca9fb987be9144898484502e0f29c
Alexei <alexeim763@gmail.com>
2018-09-12 21:16:36 -0500

    Another representation for strings

---
+++ strings/2.rb
@@ -0,0 +1,9 @@
+#String with a diferent notation
+
+s1 = %q[As Alexei said, "Ceci n´est pas une pipe"]
+s2 = %q[This is not a tab: (\t)]
+s3 = %Q[This IS a tab: (\t)]
+s4 = %Q{This Is a snowman: \u2603}
+s5 = %q<Special Characters '´{}[] in this string>
+s6 = %q:-----dasdasdasd:
+
