2ffea710bf8a9d4ed19fabda72c5a5d4c1c87a1e
Alexei <alexeim763@gmail.com>
2018-09-12 21:30:58 -0500

    finding a lenght string

---
+++ strings/3.rb
@@ -0,0 +1,9 @@
+#if you want to represent string with a multiple lines.
+
+str = "i'm a string with a
+multiple line"
+
+str = <<EOF
+I'm a string with a
+multiple line
+EOF
---
+++ strings/4.rb
@@ -0,0 +1,7 @@
+#get Large of a string
+
+str = "String"
+size_1 = str.size
+
+str = "New String"
+size_2 = str.length
