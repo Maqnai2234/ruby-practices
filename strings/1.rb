aec13cbbf71e1d33e8dec6ef5dd21163c39a4f07
Alexei <alexeim763@gmail.com>
2018-09-12 21:03:14 -0500

    Add double quote strings

--- strings/1.rb
+++ strings/1.rb
@@ -1,3 +1,12 @@
-s1 = 'This is a string'
+
+#Simplest string with a simple quote
+
+s1 = 'Hello World in Ruby'
 s2 = 'Mrs. O`Leary'
 s3 = 'Visit in documents/practices.rb'
+
+#String with a double quote
+
+s4 = "Example for a Tab: (\t)"
+s5 = "Example a another type a simple Tab: (\011)"
+s6 = "This is the Unicode snowman: \u2603 and this is a Unicode cloud: \u2601"
