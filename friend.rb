e21a29b53d44c6dd9ea04dddee227a9e603ea106
Alexei <alexeim763@gmail.com>
2018-09-12 20:44:47 -0500

    Add first Text

--- friend.rb
+++ friend.rb
@@ -1,20 +1,22 @@
 class Friend
-    @@my_name = "Fred"
-    def initialize(name, gender, phone)
-        @name, @sex, @phone = name, gender, phone
-    end
+  @@my_name = 'Fred'
+  def initialize(name, gender, phone)
+    @name = name
+    @sex = gender
+    @phone = phone
+  end
 
-    def hello
-        puts "Hi, I'am #{@name}"
-    end
+  def hello
+    puts "Hi, I'am #{@name}"
+  end
 
-    def self.our_common_friend
-        puts "Were are all friends #{@@my_name}"
-    end
+  def self.our_common_friend
+    puts "Were are all friends #{@@my_name}"
+  end
 end
 
-f1 = Friend.new("Susan", "female", "555-21232")
-f2 = Friend.new("Tom", "male", "567-21232")
+f1 = Friend.new('Susan', 'female', '555-21232')
+f2 = Friend.new('Tom', 'male', '567-21232')
 
 f1.hello
 f2.hello
