f70521a6eb66c2322dc71e32e20539769801aea7
Alexei <alexeim763@gmail.com>
2018-09-13 23:02:09 -0500

    global sub with regular expresion

--- strings/10.rb
+++ strings/10.rb
@@ -10,3 +10,10 @@ s4 = "Don't forget the spam"
 s5 = s4.sub(/spam/) {|m| m.reverse}
 
 s4.sub!(/spam/) {|m| m.reverse}
+
+##gsub = global sub, i'ts same of sub
+
+s5 = "alfalfa abracadabra"
+s6 = s5.gsub(/a[bl]/, "xx")
+
+s5.gsub!(/[lpfdbr]/) {|m| m.upcase + "-"}
