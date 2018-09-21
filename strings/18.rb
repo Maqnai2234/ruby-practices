fd04a2b311959c47f5465af18846781a16f6a72a
Alexei <alexeim763@gmail.com>
2018-09-21 10:01:01 -0500

    ...

--- strings/18.rb
+++ strings/18.rb
@@ -3,30 +3,6 @@ require 'benchmark'
 
 class String
   def levenshtein(other)
-    return nil if self.nil?
-    return nil if other.nil?
-    dm = [] #Matrix distance
-    max_item = [self.length,other.length].max
-    dm[0] = (0..max_item + 1).collect {|i| i * 1}
-    for i in 0..max_item do
-      dm[i + 1] = [i + 1] + ([0] * (dm[0].count - 1))
-    end
-    
-    for i in 1..max_item + 1 do
-      for j in 1..max_item + 1 do
-        if (j == i) && (other[i - 1] == self[j-1])
-          dm[i][j] = dm[i-1][j-1]
-        else
-          dm[i][j] = [(dm[i-1][j-1]),(dm[i-1][j]),(dm[i][j-1])].min + 1
-        end
-      end
-      # puts "STEP #{i}"
-      # print_array_bi(dm)
-    end
-    # puts "\n Se necesitan #{dm[self.length][other.length]} cambios para que #{self} se convierta en #{other}"
-  end
-
-  def levenshtein_2(other)
     other = other.to_s
     distance = Array.new(self.size + 1, 0)
     (0..self.size).each do |i|
@@ -44,7 +20,34 @@ class String
                           distance[i - 1][j - 1] + ((self[i - 1] == other[j - 1]) ? 0 : 1)].min
       end
     end
+    print_array_bi(distance)
     distance[self.size][other.size]
+  end
+
+  def levenshtein_2(other)
+    other = other.to_s
+    min = [other.length, self.length].min
+    rest = (other.length - self.length).abs
+
+    str_in = self[0..min-1]
+    other = other[0..min-1]
+    distance = Array.new(str_in.size + 1, 0)
+    (0..str_in.size).each do |i|
+      distance[i] = Array.new(other.size + 1)
+      distance[i][0] = i
+    end
+    (0..other.size).each do |j|
+      distance[0][j] = j
+    end
+    (1..str_in.size).each do |i|
+      (1..other.size).each do |j|
+        distance[i][j] = [distance[i - 1][j] + 1,
+                          distance[i][j - 1] + 1,
+                          distance[i - 1][j - 1] + ((str_in[i - 1] == other[j - 1]) ? 0 : 1)].min
+      end
+    end
+    print_array_bi(distance)
+    distance[str_in.size][other.size] + (rest)
   end
 end
 
@@ -60,6 +63,6 @@ def print_array_bi(arr)
 end
 
 Benchmark.bm do |bm|
-  bm.report { "a".levenshtein("sdfasdasdasdsd") }
-  bm.report { "a".levenshtein_2("sdfasdasdasdsd") }
+  bm.report { puts "aaaaa".levenshtein("ba") }
+  bm.report { puts "a".levenshtein_2("sdfasdasdasdsd") }
 end
