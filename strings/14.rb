78a7bfdcfbf2c9998b4e3afab5ad0a3e70ad6200
Alexei <alexeim763@gmail.com>
2018-09-20 20:43:45 -0500

    add a new example

--- strings/14.rb
+++ strings/14.rb
@@ -3,6 +3,8 @@
 def rot13(str)
   str.tr("A-Ma-mN-Zn-z", "N-Zn-zA-Ma-m")
 end
-
-puts rot13("Hola A todos")
+string = "Hola A todos"
+puts rot13(string)
 puts rot13("Ubyn N gbqbf")
+
+puts rot13(rot13(string))
